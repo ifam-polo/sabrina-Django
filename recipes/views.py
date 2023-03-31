c207133b2165b989016fd239b64f2e34e06f7bb6
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-03-31 11:49:42 -0400

    adicionado font-awesome no home.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html', context={
+        'name':'Sabrina'})
 
