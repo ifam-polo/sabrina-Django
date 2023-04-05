eb0d98845d5bb29b13c24ad49d7faca45da5284e
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-04-05 11:36:20 -0400

    separando o template de home e recipes, e criando herança em templates e blocks

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name':'Sabrina'})
 
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe_view.html', context={
+        'name': 'Sabrina Frazão',
+    })
