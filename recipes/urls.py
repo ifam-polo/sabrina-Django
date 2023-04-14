b13fb4c0b2eedc4d14a30191f17281a3026329f8
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-04-14 15:05:05 -0400

    Registrando category na admin

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="recipes-home",),
-    path('recipes/<int:id>/', views.recipe, name="recipes:recipe"),
+    path('', views.home, name="home",),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
