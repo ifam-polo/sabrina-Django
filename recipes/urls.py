36435a4a2cdfbbd60ebdba83189cc9e1e1a9ee24
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-04-18 10:23:52 -0400

    Organizando as urls do projeto e recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 app_name = 'recipes'
 
@@ -10,8 +8,3 @@ urlpatterns = [
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, 
-                      document_root = settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, 
-                      document_root = settings.STATIC_ROOT)
