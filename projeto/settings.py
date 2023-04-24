62374944967596591b31c3aee62eae5bfe7787db
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-04-24 12:03:14 -0400

    refazendo as migrações dos models

--- projeto/settings.py
+++ projeto/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # A gente tem apps também
     'recipes',
 ]
 
