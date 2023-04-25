23378c7148a76bb4a5c79d743ea352add84ef75a
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-04-25 13:06:39 -0400

    refanzendo as migrações

--- recipes/models.py
+++ recipes/models.py
@@ -19,7 +19,7 @@ class Recipe(models.Model):
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=None)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
