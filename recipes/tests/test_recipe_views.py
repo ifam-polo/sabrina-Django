20c67f2b5abd36b4d51bdcaa5508ebb3b631c53a
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-05-08 12:50:17 -0400

    criando fixtures para um teste único

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-from django.urls import reverse, resolve
-from recipes import views 
+from django.urls import resolve, reverse
+from recipes import views
 from recipes.models import Category, Recipe, User
+
 
 class RecipeViewsTest(TestCase):
     def test_recipe_home_views_function_is_correct(self):
@@ -12,7 +13,7 @@ class RecipeViewsTest(TestCase):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
-    def test_recipe_home_view_loads_correct(self):
+    def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
@@ -23,8 +24,32 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here 🥲</h1>',
             response.content.decode('utf-8')
         )
-
     
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name = 'user',
+            last_name = 'name',
+            username = 'Username',
+            password = '123456',
+            email = 'username@gmsil.com',
+        )
+        recipe = Recipe.objects.create(
+            category = category,
+            author = author,
+            title = 'Recipe title',
+            description = 'recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 10,
+            servings_unit = 'Porções',
+            preparation_steps = 'Recipe Preparation Steps',
+            preparation_steps_is_html =False ,
+            is_published =True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        pass
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
