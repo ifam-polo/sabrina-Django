5043e35d2e8b7f06f7eb95f2a86e0b77b87008c4
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-05-10 11:33:08 -0400

    testes nos category, verificando recipes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,9 +1,7 @@
-from .test_recipe_base import RecipeTestBase, Recipe
+from .test_recipe_base import RecipeTestBase
 from django.urls import resolve, reverse
 from recipes import views
 from recipes.models import Category, Recipe, User
-
-
 
 
 class RecipeViewsTest(RecipeTestBase):
@@ -34,6 +32,7 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
+
         self.assertIn('Recipe Title', content)
         self.assertEqual(len(response_context_recipes),1)
      
@@ -45,6 +44,16 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'this is a category test'
+        self.make_recipe(title= 'this is a category test')
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('this is a category test', content)
+        self.assertEqual(len(response_context_recipes),1)
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
