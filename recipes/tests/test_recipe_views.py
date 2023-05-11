4f45fc3c92ec4b01df1f73d97acf59f4a7750348
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-05-11 08:19:54 -0400

    Testando se category e detail carregam a receita correta

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -45,15 +45,14 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
 
-    def test_recipe_category_template_loads_recipes(self):
+    def test_recipe_category_template_loads_correct_recipes(self):
         needed_title = 'this is a category test'
-        self.make_recipe(title= 'this is a category test')
+        self.make_recipe(title= needed_title)
         response = self.client.get(reverse('recipes:category', args=(1,)))
         content = response.content.decode('utf-8')
-        response_context_recipes = response.context['recipes']
 
-        self.assertIn('this is a category test', content)
-        self.assertEqual(len(response_context_recipes),1)
+        self.assertIn(needed_title, content)
+  
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
@@ -65,5 +64,11 @@ class RecipeViewsTest(RecipeTestBase):
 
     
 
-
+    def test_recipe_detail_template_loads_correct_recipes(self):
+        needed_title = 'this is a detail page - it load one recipe'
+        self.make_recipe(title= needed_title)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id':1}))
+        content = response.content.decode('utf-8')
+      
+        self.assertIn(needed_title, content)
    
