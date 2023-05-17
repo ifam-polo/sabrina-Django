95301aae953892fe767dae7d774fe13135fcc0e1
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-05-17 15:14:33 -0400

    adicionando css na paginação

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,4 +1,6 @@
 import math
+
+
 def make_pagination_range(
     page_range,
     qty_pages,
@@ -7,12 +9,13 @@ def make_pagination_range(
     middle_range = math.ceil(qty_pages/2)
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
+
     total_pages = len(page_range)
 
     start_range_offset = abs(start_range) if start_range < 0 else 0
 
     if start_range < 0:
-        start_range=0
+        start_range = 0
         stop_range += start_range_offset
 
     if stop_range >= total_pages:
@@ -30,5 +33,3 @@ def make_pagination_range(
             'first_page_out_of_range': current_page > middle_range,
             'last_page_out_of_range': stop_range < total_pages,
     }
-
-
