84e179dc8601e4319bbae6878e6ea5ab536108e4
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-05-18 13:10:13 -0400

    configurando django messages

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.pagination import make_pagination
 from recipes.models import Recipe
 import os
+from django.contrib import messages
+
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -12,6 +14,8 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
+
+    messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
