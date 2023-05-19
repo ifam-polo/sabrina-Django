53435d533d687f635acc5eb3109086ff3fcd6365
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-05-19 16:19:07 -0400

    criando views do formulario

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+from django.http import Http404
 
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+# flake8:noqa
 from django.contrib.auth.models import User
 from django.db import models
 
