d2fe57cad2ff2b49d6d2569a406952ccb4a6a6ba
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-05-26 10:45:17 -0400

    fazendo autenticação do usuario

--- authors/test/test_author_register_form.py
+++ authors/test/test_author_register_form.py
@@ -145,10 +145,13 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
               
     def test_email_field_must_be_unique(self):
         url = reverse('authors:register_create')
+
         self.client.post(url, data=self.form_data, follow=True)
         response = self.client.post(url, data=self.form_data, follow=True)
+
         msg = 'User e-mail is already in use'
         self.assertIn(msg, response.context['form'].errors.get('email'))
+        print(response.context)
         self.assertIn(msg, response.content.decode('utf-8'))
 
     def test_author_created_can_login(self):
