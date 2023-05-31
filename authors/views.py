54eb973dfddd6cc33bc970287750d202e832fbf2
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-05-31 14:30:39 -0400

    Criando a pagina inicial da dashboard

--- authors/views.py
+++ authors/views.py
@@ -1,10 +1,11 @@
-from django.http import Http404
-from django.shortcuts import redirect, render
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, RegisterForm
-from django.contrib import messages
+from django.http import Http404
+from django.shortcuts import redirect, render
 from django.urls import reverse
+
+from .forms import LoginForm, RegisterForm
 
 
 def register_view(request):
@@ -12,7 +13,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
-        'form_action': reverse('authors:register_create')
+        'form_action': reverse('authors:register_create'),
     })
 
 
@@ -23,14 +24,14 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
-    
+
     if form.is_valid():
         user = form.save(commit=False)
         user.set_password(user.password)
         user.save()
-        messages.success(request, 'Your user is created, please log in')
-        
-        del (request.session['register_form_data'])
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
         return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
@@ -49,7 +50,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -65,19 +65,24 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
     if not request.POST:
-        messages.error(request, "Invalid logout request")
-        return redirect(reverse('authors:login'))
-    
-    if request.POST.get('username') != request.user.username:
-        messages.error(request, "Invalid logout user")
+        messages.error(request, 'Invalid logout request')
         return redirect(reverse('authors:login'))
 
-    messages.error(request, "Logged out successfully")
+    if request.POST.get('username') != request.user.username:
+        messages.error(request, 'Invalid logout user')
+        return redirect(reverse('authors:login'))
+
+    messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
