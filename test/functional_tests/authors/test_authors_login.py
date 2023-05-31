1c399e83fbdac428a163426e6cbf722e01299e71
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-05-31 13:27:39 -0400

    Testando logout

--- test/functional_tests/authors/test_authors_login.py
+++ test/functional_tests/authors/test_authors_login.py
@@ -2,7 +2,7 @@ import pytest
 from django.contrib.auth.models import User
 from django.urls import reverse
 from selenium.webdriver.common.by import By
-from time import sleep
+
 
 from .base import AuthorsBaseTest
 
@@ -36,4 +36,37 @@ class AuthorsLoginTest(AuthorsBaseTest):
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
         
-        sleep(4)
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(
+                self.live_server_url +
+                reverse('authors:login_create')
+        )
+
+        self.assertIn(
+                'Not Found',
+                self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+            
+    def test_form_login_invalid_credentials(self):
+        # Usuário abre a página de login
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+
+        # Usuário vê o formulário de login
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # E tenta enviar valores com dados que não correspondem
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys('invalid_user')
+        password.send_keys('invalid_password')
+
+        # Envia o formulário
+        form.submit()
+
+        # Vê uma mensagem de erro na tela
+        self.assertIn(
+            'Invalid credentials',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
