39030b050a322a8fb8d23772bfcc1dff1e0a710c
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-06-02 18:39:17 -0400

    Category e Search ListView

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('recipes/search/', views.search, name="search"),
-    path('recipes/category/<int:category_id>/', views.category, 
-         name="category"),
-    path('recipes/<int:id>/', views.recipe, name="recipe"), 
-   
-]
+    path('', views.RecipeListViewHome.as_view(), name="home"),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(), name="search"
+    ),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(), name="category"
+    ),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 import os
-from typing import List
 
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.generic import ListView
 
 from recipes.models import Recipe
 from utils.pagination import make_pagination
@@ -11,7 +11,7 @@ from utils.pagination import make_pagination
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
-class RecipeListViewBase(List):
+class RecipeListViewBase(ListView):
     model = Recipe
     context_object_name = 'recipes'
     ordering = ['-id']
@@ -34,6 +34,48 @@ class RecipeListViewBase(List):
         ctx.update(
             {'recipes': page_obj, 'pagination_range': pagination_range}
         )
+        return ctx
+
+
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            category__id=self.kwargs.get('category_id')
+        )
+        return qs
+
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            )
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+
+        ctx.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
         return ctx
 
 
