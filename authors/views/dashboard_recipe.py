1335a81fadade9a0e12f423aad3f79aa99434074
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-06-02 14:23:31 -0400

    criando o método post

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -9,41 +9,51 @@ from recipes.models import Recipe
 
 
 class DashboardRecipe(View):
-    def get(self, request, id):
-        recipe = Recipe.objects.filter(
-            is_published=False,
-            author=request.user,
-            pk=id,
-        ).first()
+    def get_recipe(self, id):
+        recipe = None
 
-        if not recipe:
-            raise Http404()
+        if id:
+            recipe = Recipe.objects.filter(
+                is_published=False,
+                author=self.request.user,
+                pk=id,
+            ).first()
 
-        form = AuthorRecipeForm(
-            data=request.POST or None,
-            files=request.FILES or None,
-            instance=recipe
-        )
+            if not recipe:
+                raise Http404()
 
-        if form.is_valid():
-            # Agora, o form é válido e eu posso tentar salvar
-            recipe = form.save(commit=False)
+        return recipe
 
-            recipe.author = request.user
-            recipe.preparation_steps_is_html = False
-            recipe.is_published = False
-
-            recipe.save()
-
-            messages.success(request, 'Sua receita foi salva com sucesso!')
-            return redirect(
-                reverse('authors:dashboard_recipe_edit', args=(id,))
-            )
-
+    def render_recipe(self, form):
         return render(
-            request,
+            self.request,
             'authors/pages/dashboard_recipe.html',
             context={
                 'form': form
             }
         )
+
+    def get(self, request, id):
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+        return self.render_recipe(form)
+
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(
+            data=request.POST or None,
+            files=request.FILES or None,
+            instance=recipe
+        )
+        if form.is_valid():
+            # Agora, o form é válido e eu posso tentar salvar
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.preparation_steps_is_html = False
+            recipe.is_published = False
+            recipe.save()
+            messages.success(request, 'Sua receita foi salva com sucesso!')
+            return redirect(
+                reverse('authors:dashboard_recipe_edit', args=(id,))
+            )
+            return self.render_recipe(form)
