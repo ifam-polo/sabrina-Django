5379fe2597ca1e774eec27775a6fd99b5501aa18
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-06-07 09:21:43 -0400

    Criando a URL e Template para profile

--- authors/urls.py
+++ authors/urls.py
@@ -26,6 +26,11 @@ urlpatterns = [
         views.DashboardRecipe.as_view(),
         name='dashboard_recipe_edit'
     ),
+    path(
+        'profile/<int:id>/',
+        views.ProfileView.as_view(),
+        name='profile'
+    ),
 
 
 ]
